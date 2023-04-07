c1a26fa3e21e5e3209d0d37e6560d891cef4b904
murgraydj <murgraydj@gmail.com>
2023-04-07 13:59:33 -0400

    Creates Knight_travails, Board, and Node classes
    The Board and Node classes are currently stored in the Knight_travails
    class.

--- knight_travails.rb
+++ knight_travails.rb
@@ -1,3 +1,43 @@
 #Author: Murgray D. John
 #Date: 4/7/2023
 #Purpose: Shows the shortest way for a knight to get from one square to another.
+
+class Knight_Travails
+  class Board
+    attr_reader :start_node
+    attr_accessor :history
+
+    def initialize(start_pos)
+      start_node = Node.new(start_pos)
+    end
+  end
+
+  class Node
+      attr_accessor :coordinate
+      attr_accessor :parent
+      attr_accessor :children
+
+      MOVES = [[-1,-2], [-1,2], 
+                [1,-2], [1,2], 
+                [-2,-1], [-2,1], 
+                [2,-1], [2,1]].freeze
+
+      def initialize(coordinate, parent=nil)
+        @coordinate = coordinate
+        @parent = parent
+        @children = []
+      end
+
+      def create_children(parent)
+        children = []
+        
+      end
+  end
+
+  attr_accessor :board
+  attr_accessor :knight
+
+  def initialize(start_pos, end_pos)
+    @board = create_board(start_pos)
+  end
+end
